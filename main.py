d60d2267417eb392ccf3f7abd84b8dbd8ee491d3
afterschool-treetree2 <62988945+afterschool-treetree2@users.noreply.github.com>
2025-04-03 22:05:41 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Constroi um programa que escreve permite ao utlizador fazer o seguinte:
+* Adicionar coisas à lista de compras guardada em compras.txt
+* Verificar se um certo elemento está na lista de compras, e se sim, dizer que quantidade.
+
+Repara que no ficheiro os produtos devem ser guardados como:
+[produto] [quantidade]
+'''
